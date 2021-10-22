39b186f6394141fb0cab00fdaa5200e864445038
renzotw <renzo.atw@gmail.com>
2021-10-22 10:24:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,17 @@ app = Flask(__name__)
 @app.route("/")
 # simplest possible approach
 def main():
-    return "hi there!"
+    return render_template("main.html") # Searches in template directory for main.html
+
+@app.route("/submit/", methods = ["POST", "GET"])
+def submit():
+    if request.method == "GET":
+        return render_template("submit.html")
+    else:
+        try:
+            render_template("submit.html", 
+                             name = request.form["name"],
+                             student = request.form["student"])
+        except:
+            render_template("submit.html")
+    
