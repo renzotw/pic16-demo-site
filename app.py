ea7d6d3eb26a53cb7e470fe1278779f4595aa325
renzotw <renzo.atw@gmail.com>
2021-10-22 15:08:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, current_app, g, render_template, redirect, request, flash, url_for, session, Flask
+from flask import Flask, g, render_template, request
 
 import sqlite3
 import click
@@ -19,43 +19,38 @@ def submit():
         return render_template("submit.html")
     else:
         try:
-            render_template("submit.html", 
-                             name = request.form["name"],
-                             message = request.form["message"])
+            return render_template("submit.html", name = request.form["name"], message = request.form["message"])
         except:
-            render_template("submit.html")
-
-    if request.method == "POST":
-        insert_message(request)
-        return render_template("submit.html")
+            return render_template("submit.html")
 
 
-def get_message_db():
-    if 'db' not in g:
-        g.message_db = sqlite3.connect('message_db.sqlite')
+
+# def get_message_db():
+#     if 'db' not in g:
+#         g.message_db = sqlite3.connect('message_db.sqlite')
         
-    conn = g.message_db
-    cursor = conn.cursor()
+#     conn = g.message_db
+#     cursor = conn.cursor()
 
-    # Create table if it doesn't exist
-    cursor.execute("CREATE TABLE IF NOT EXISTS messages (ID INTEGER PRIMARY KEY AUTOINCREMENT, messages TEXT, name TEXT)")
-    return g.message_db   
+#     # Create table if it doesn't exist
+#     cursor.execute("CREATE TABLE IF NOT EXISTS messages (ID INTEGER PRIMARY KEY AUTOINCREMENT, messages TEXT, name TEXT)")
+#     return g.message_db   
 
 
-def insert_message(request):
-    message = request.form["message"]
-    name = request.form["name"]
+# def insert_message(request):
+#     message = request.form["message"]
+#     name = request.form["name"]
 
-    db = get_message_db()
+#     db = get_message_db()
 
-    db.execute(
-                'INSERT INTO message (messages, name) VALUES (?, ?)',
-                (username, salt, generate_password_hash(password + salt))
-            )
+#     db.execute(
+#                 'INSERT INTO message (messages, name) VALUES (?, ?)',
+#                 (username, salt, generate_password_hash(password + salt))
+#             )
 
-    db.commit()
+#     db.commit()
 
-    conn.close()
+#     conn.close()
 
 
 
@@ -64,30 +59,30 @@ def insert_message(request):
 
 # Miscellaneous
 
-message_bp = Blueprint('message', __name__, url_prefix='/message')
+# message_bp = Blueprint('message', __name__, url_prefix='/message')
 
-@app.route("/submit/", methods = ["POST", "GET"])
-def get_message_db():
-    if 'db' not in g:
-        g.message_db = sqlite3.connect('users.sqlite')
+# @app.route("/submit/", methods = ["POST", "GET"])
+# def get_message_db():
+#     if 'db' not in g:
+#         g.message_db = sqlite3.connect('users.sqlite')
 
-    with current_app.open_resource('init.sql') as f:
-        g.message_db.executescript(f.read().decode('utf8'))
+#     with current_app.open_resource('init.sql') as f:
+#         g.message_db.executescript(f.read().decode('utf8'))
 
-    return g.message_db
+#     return g.message_db
 
-def insert_message(request):
-    if request.method == 'POST':
-        message = request.form["message"]
-        name = request.form["name"]
-        db = get_message_db()
-        error = None
-        user = db.execute(
-            "INSERT INTO messages (id, message, name) VALUES (?,?,?)"
-        )
-        db.commit()
-        flash("Message submitted successfully!")
-    flash(error)
+# def insert_message(request):
+#     if request.method == 'POST':
+#         message = request.form["message"]
+#         name = request.form["name"]
+#         db = get_message_db()
+#         error = None
+#         user = db.execute(
+#             "INSERT INTO messages (id, message, name) VALUES (?,?,?)"
+#         )
+#         db.commit()
+#         flash("Message submitted successfully!")
+#     flash(error)
    
 
 
