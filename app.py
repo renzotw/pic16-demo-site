0756bb3a4ee8ea79a549333b6ab224c506fb9769
renzotw <renzo.atw@gmail.com>
2021-10-22 11:01:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g, Blueprint
+
+import sqlite3
+import click
+
+import random 
+import string
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,4 +24,17 @@ def submit():
                              student = request.form["student"])
         except:
             render_template("submit.html")
+
+message_bp = Blueprint('message', __name__, url_prefix='/message')
+
+def get_message_db():
+    if 'db' not in g:
+        g.message_db = sqlite3.connect('users.sqlite')
+
+    cursor = g.message_db.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS messages AS SELECT id, handle, message;")
+
+    return g.auth_db
+
+
     
