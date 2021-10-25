e0803efde87960c3aaf08b0a9da23be4399e82bb
renzotw <renzo.atw@gmail.com>
2021-10-24 21:18:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,10 @@ def submit():
         return render_template("submit.html")
     else:
         try:
-            return render_template("submit.html", name = request.form["name"], message = request.form["message"])
+            insert_message(request)
+            return render_template("submit.html", name=request.form['name'], message=request.form['message'])
         except:
             return render_template("submit.html")
-
-
 
 def get_message_db():
     if 'db' not in g:
@@ -43,47 +42,19 @@ def insert_message(request):
     handle = request.form["name"]
 
     db = get_message_db()
+    error = None
 
-    db.execute("INSERT INTO messages (handle, message) VALUES (?,?);")
-    db.commit()
+    if not message:
+        error = "message is required."
+    elif not handle:
+        error = "name is required."
 
-    conn.close()
+    if error is None:
+        db.execute("INSERT INTO messages (handle, message) VALUES (?,?)", (handle, message))
+        db.commit()
+
+    db.close()
 
     return message, handle
-
-
-
-
-
-
-# Miscellaneous
-
-# message_bp = Blueprint('message', __name__, url_prefix='/message')
-
-# @app.route("/submit/", methods = ["POST", "GET"])
-# def get_message_db():
-#     if 'db' not in g:
-#         g.message_db = sqlite3.connect('users.sqlite')
-
-#     with current_app.open_resource('init.sql') as f:
-#         g.message_db.executescript(f.read().decode('utf8'))
-
-#     return g.message_db
-
-# def insert_message(request):
-#     if request.method == 'POST':
-#         message = request.form["message"]
-#         name = request.form["name"]
-#         db = get_message_db()
-#         error = None
-#         user = db.execute(
-#             "INSERT INTO messages (id, message, name) VALUES (?,?,?)"
-#         )
-#         db.commit()
-#         flash("Message submitted successfully!")
-#     flash(error)
-   
-
-
 
     
